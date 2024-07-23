8e59e997ea51db5a7a6b7154e11cd717be142816
Suryaspt <suryanadiasaputra12@gmail.com>
2024-07-23 14:32:24 +0800

    menambahkan judul

--- layout/header.php
+++ layout/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE</title>
+    <title>AdminLTE 3</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
